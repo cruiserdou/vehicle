53d877da597deb16142e953d9d77c1dfe865e3a4
xwq-mac <cruiserdou@qq.com>
2014-06-24 21:53:17 +0800

    ok

--- src/main/webapp/static/app/auto/view/contract/download/Grid.js
+++ src/main/webapp/static/app/auto/view/contract/download/Grid.js
@@ -8,14 +8,13 @@ Ext.define('App.view.contract.download.Grid', {
         this.columns = [
             {text: '会议编号', width: 120, dataIndex: 'id'},
             {text: '会议名称', width: 130, dataIndex: 'name', sortable: true},
-            {text: '工程名称', width: 130, dataIndex: 'project'},
             {text: '上传日期', width: 100, dataIndex: 'sign',
                 renderer: Ext.util.Format.dateRenderer('Y-m-d')
             },
-            {text: '下载', width: 80, dataIndex: 'doc_nm',
+            {text: '下载', width: 80, dataIndex: 'project',
              renderer: function(val){
-                 return Ext.String.format('<a href="upload/contract/' + val +
-                     '" target="_blank"><img src="/teldata/css/images/cloud-download.png" /></a>')
+                 return Ext.String.format('<a href="static/upload/' + val +
+                     '" target="_blank"><img src="static/css/images/cloud-download.png" /></a>')
              }
             },
             {text: '备注',  flex: 1, dataIndex: 'remark'}
